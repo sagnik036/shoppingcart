652aa925a0770aeedcea4035605e15cbe9b95dc7
sagnik036 <iamsagnikbanik@gmail.com>
2022-10-29 04:24:02 +0530

    v2

--- shoppingcart/authentication.py
+++ shoppingcart/authentication.py
@@ -1,9 +1,6 @@
 from django.contrib.auth import get_user_model, authenticate
 from django.http import HttpRequest
 from django.contrib.auth.backends import BaseBackend
-from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
-from dj_rest_auth.registration.views import SocialLoginView
-from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from django.conf import settings
 
 class MyCustomAuthBackend(BaseBackend):
